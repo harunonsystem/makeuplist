3ad4f9a900fdefc8d003f2a737df0f947172bac0
harunon <harunonsystem@gmail.com>
2020-07-19 00:40:31 +0900

    git commit -m "feat: FloatingActionButton "

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:makeup/auth.dart';
 import 'package:splashscreen/splashscreen.dart';
+
+import 'auth.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -39,33 +40,34 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pinkAccent,
-          centerTitle: true,
-          title: Text('MakeupList'),
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: <Color>[Colors.blueAccent, Colors.pinkAccent])),
-          ),
-          automaticallyImplyLeading: false,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {
-                Navigator.of(context).pushNamed('/root');
-                // if(FirebaseAuth.instance.currentUser() != null){
-                //   Navigator.of(context).pushNamed('/profile');
-                // }else{
-                //   Navigator.of(context).pushNamed('/login');
-                // }
-              },
-            )
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
+        title: Text('MakeupList'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[Colors.blueAccent, Colors.pinkAccent])),
         ),
-        body: StreamBuilder<QuerySnapshot>(
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).pushNamed('/root');
+              // if(FirebaseAuth.instance.currentUser() != null){
+              //   Navigator.of(context).pushNamed('/profile');
+              // }else{
+              //   Navigator.of(context).pushNamed('/login');
+              // }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
             stream: Firestore.instance
                 .collection('users')
                 .document(uid)
@@ -80,7 +82,11 @@ class _HomePageState extends State<HomePage> {
                   itemBuilder: (context, index) {
                     return Padding(
                       padding: const EdgeInsets.fromLTRB(80, 10, 80, 10),
-                      child: Card(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pushNamed('/create');
+                        },
+                        child: Card(
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(30.0)),
                           elevation: 10,
@@ -126,14 +132,134 @@ class _HomePageState extends State<HomePage> {
                                 ),
                               ),
                             ),
-                         ), 
-                         ),
+                          ),
+                        ),
+                      ),
                     );
                   },
                   itemCount: snapshot.data.documents.length,
                 );
               }
-            })
-        );
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (uid != null) {
+            return _createDialog();
+          } else {
+            return _needLogin();
+          }
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+
+  _needLogin() async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('ユーザー登録が必要になります',
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),
+            content: RaisedButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: Text('登録する', style: TextStyle(color: Colors.white),),
+              onPressed: () {
+                Navigator.pushNamed(context, '/root');
+              },
+              color: Colors.pinkAccent,
+            ),
+          );
+        });
+  }
+
+  _createDialog() async {
+    TextEditingController listTitleController = TextEditingController();
+    TextEditingController listSubtitleController = TextEditingController();
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              'リストのタイトルを入力✌️',
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            content: Builder(builder: (context) {
+              var height = MediaQuery.of(context).size.height / 6;
+              var width = MediaQuery.of(context).size.width / 2;
+              return Container(
+                height: height,
+                width: width,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: listTitleController,
+                      decoration: InputDecoration(hintText: 'list title...'),
+                    ),
+                    TextFormField(
+                      controller: listSubtitleController,
+                      decoration: InputDecoration(hintText: 'list subtitle...'),
+                    ),
+                  ],
+                ),
+              );
+            }),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  print(listTitleController.text);
+                  print(listSubtitleController.text);
+                  // Firestore.instance
+                  //     .collection('users')
+                  //     .document(uid)
+                  //     .collection('list')
+                  //     .add({
+                  //   'title': listTitleController.text,
+                  //   'subtitle': listSubtitleController.text,
+                  // });
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
   }
 }
+// GestureDetector(
+//     onTap: () {
+//       Navigator.of(context).pushNamed('/create');
+//     },
+//     child: Card(
+//         shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(30.0)),
+//         elevation: 10,
+//         child: Container(
+//             decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(10.0),
+//                 gradient: LinearGradient(
+//                     begin: Alignment.topLeft,
+//                     end: Alignment.bottomRight,
+//                     colors: [Colors.greenAccent, Colors.green])),
+//             width: MediaQuery.of(context).size.width / 2,
+//             height: MediaQuery.of(context).size.height / 12,
+//             child: Column(
+//               children: <Widget>[
+//                 Padding(
+//                   padding: const EdgeInsets.all(16.0),
+//                   child: Center(
+//                     child: const Text(
+//                       'リストに追加',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: 18.0,
+//                       ),
+//                       textAlign: TextAlign.center,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ))))
